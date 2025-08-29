0cef203b2c4d04e0959a6559931b82bf1d90ac69
Thanos Sotiropoulos <thasotiropoulos@gmail.com>
2025-08-29 18:56:22 +0300

    added list of prompts for history and a flag argument

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key = api_key)
 
+
+
+
+
 n = len(sys.argv)
 
 if n < 2:
@@ -18,15 +22,24 @@ if n < 2:
 
 
 # Prompt the LLM from sys.argv and get a response, print the response and token costs in the terminal. 
-input_prompt = str(sys.argv[1]) #sys.argv[0] is the first argument which is the program called, sys.argv[1] is the first argument converted to a string. 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=input_prompt)
+user_prompt = str(sys.argv[1]) #sys.argv[0] is the first argument which is the program called, sys.argv[1] is the first argument converted to a string. 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+prompt_tokens = response.usage_metadata.prompt_token_count
+response_tokens = response.usage_metadata.candidates_token_count
 
-print(response.text) # print the .text part of the bot's response 
+if "--verbose" in sys.argv:#len(sys.argv) > 2 and str(sys.argv[2]) == "--verbose": #if --verbose flag exists, print a lot of info, otherwise just the response.
+    
+    print(f"User prompt: {user_prompt}\n")
+    print(response.text) # print the .text part of the bot's response 
 
-# print the token counts for the prompt and the response
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}") 
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
+    # print the token counts for the prompt and the response
+    print(f"Prompt tokens: {prompt_tokens}") 
+    print(f"Response tokens: {response_tokens}")
+else: 
+    print(response.text) # print the .text part of the bot's response 
 
 
 # def main():
